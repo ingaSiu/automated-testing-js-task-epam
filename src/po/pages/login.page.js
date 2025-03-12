03be923f1a7cfa47379cb34994ee43b463fb7443
ingaSiu <ingasiud@gmail.com>
2025-03-12 14:57:05 +0200

    create login page

---
+++ src/po/pages/login.page.js
@@ -0,0 +1,9 @@
+const LoginForm = require('./../components/login/login-form.component');
+
+class LoginPage {
+  constructor() {
+    this.loginForm = new LoginForm();
+  }
+}
+
+module.exports = LoginPage;

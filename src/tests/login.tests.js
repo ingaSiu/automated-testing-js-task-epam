f847a733f278cc601a1d37c821fde572f078a447
ingaSiu <ingasiud@gmail.com>
2025-03-13 18:33:08 +0200

    change solution, because the commented parts did not work correctly on chrome browser

--- src/tests/login.tests.js
+++ src/tests/login.tests.js
@@ -1,5 +1,6 @@
 const LoginPage = require('./../po/pages/login.page');
 const DashboardPage = require('./../po/pages/dashboard.page');
+const { Key } = require('webdriverio');
 
 describe('Login page', () => {
   let loginPage;
@@ -9,34 +10,47 @@ describe('Login page', () => {
     await loginPage.open();
   });
 
-  it('Test Login form with empty credentials', async () => {
+  it('should not login with empty inputs', async () => {
     const { loginForm } = loginPage;
 
     await loginForm.input('username').setValue('Username');
     await loginForm.input('password').setValue('Password');
 
-    await loginForm.input('username').clearValue();
-    await loginForm.input('password').clearValue();
+    // await loginForm.input('username').setValue('');
+    // await loginForm.input('password').clearValue();
+
+    await loginForm.input('username').click();
+    await browser.keys([Key.Control, 'a']);
+    await browser.keys([Key.Backspace]);
+
+    await loginForm.input('password').click();
+    await browser.keys([Key.Control, 'a']);
+    await browser.keys([Key.Backspace]);
+    await loginForm.input('username').click();
 
     await loginForm.loginBtn.click();
 
-    await expect(loginForm.errorElement.toHaveText('Epic sadface: Username is required'));
+    await expect(loginForm.errorElement).toHaveText('Epic sadface: Username is required');
   });
 
-  it('Test Login form with credentials by passing Username', async () => {
+  it('should not login without password', async () => {
     const { loginForm } = loginPage;
 
-    await loginForm.input('username').setValue('standard_user');
+    await loginForm.input('username').setValue('user');
     await loginForm.input('password').setValue('secret_sauce');
 
-    await loginForm.input('password').clearValue();
+    //await loginForm.input('password').clearValue();
+
+    await loginForm.input('password').click();
+    await browser.keys([Key.Control, 'a']);
+    await browser.keys([Key.Backspace]);
 
     await loginForm.loginBtn.click();
 
-    await expect(loginForm.errorElement.toHaveText('Epic sadface: Password is required'));
+    await expect(loginForm.errorElement).toHaveText('Epic sadface: Password is required');
   });
 
-  it('Test Login form with credentials by passing Username & Password', async () => {
+  it('should login with credentials and navigate to dashboard page', async () => {
     const { loginForm } = loginPage;
 
     await loginForm.input('username').setValue('standard_user');
@@ -45,6 +59,6 @@ describe('Login page', () => {
 
     const dashboardPage = new DashboardPage();
 
-    await expect(dashboardPage.header.appLogo.toHaveText('Swag Labs'));
+    await expect(dashboardPage.header.appLogo).toHaveText('Swag Labs');
   });
 });

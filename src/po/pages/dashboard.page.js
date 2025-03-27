0817c83fcbc7c358c2dfc75a1dcc8ef83585d987
ingaSiu <ingasiud@gmail.com>
2025-03-27 13:02:40 +0200

    extend base page

--- src/po/pages/dashboard.page.js
+++ src/po/pages/dashboard.page.js
@@ -1,7 +1,9 @@
 const Header = require('./../components/common/header.component');
+const BasePage = require('./basepage');
 
-class DashboardPage {
+class DashboardPage extends BasePage {
   constructor() {
+    super('inventory.html');
     this.header = new Header();
   }
 }

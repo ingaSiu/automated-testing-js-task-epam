bc842be23f4bac1599ca05e88f10b7e0ea9a3d3b
ingaSiu <ingasiud@gmail.com>
2025-04-04 10:22:12 +0300

    add action methods

--- src/po/components/common/header.component.js
+++ src/po/components/common/header.component.js
@@ -20,6 +20,16 @@ class HeaderComponent extends BaseComponent {
   get logoutLink() {
     return $('a#logout_sidebar_link');
   }
+
+  async openSideNav() {
+    await this.burgerMenuBtn.click();
+    await this.sideNav.waitForDisplayed();
+  }
+
+  async logout() {
+    await this.openSideNav();
+    await this.logoutLink.click();
+  }
 }
 
 module.exports = HeaderComponent;

dc967f2997d5519a24b15fc763ef55695368f37d
ingaSiu <ingasiud@gmail.com>
2025-04-12 07:50:02 +0300

    add header and footer

--- src/po/pages/basepage.js
+++ src/po/pages/basepage.js
@@ -1,6 +1,11 @@
+const Header = require('../components/common/header.component');
+const Footer = require('../components/common/footer.component');
+
 class BasePage {
   constructor(path = '') {
     this.path = path;
+    this.header = new Header();
+    this.footer = new Footer();
   }
   async open(path = '') {
     await browser.url(`/${this.path}`);
